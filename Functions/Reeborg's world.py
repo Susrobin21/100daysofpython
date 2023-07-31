fb132a07763d1fd8d42751cd07eabd1ec2b26589
Rupesh <98076862+Susrobin21@users.noreply.github.com>
2023-08-01 00:14:11 +0530

    Add files via upload

---
+++ Functions/Reeborg's world.py
@@ -0,0 +1,17 @@
+def right():
+    turn_left()
+    turn_left()
+    turn_left()
+turn_left()
+move()
+right()
+move()
+right()
+move()
+right()
+move()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
